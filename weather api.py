b07dcf02ce3bda341ec6753e945c35bf30511995
Vishesh Tripathi <33310569+12VISHESH@users.noreply.github.com>
2019-08-09 18:20:20 +0530

    Add files via upload

---
+++ weather api.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import requests
+api_address ='insert weather api in this place . its is unique id fpr every user '
+city=input("enter the city name:")
+url=api_address+city
+json_data=requests.get(url).json()
+print(json_data)
+
